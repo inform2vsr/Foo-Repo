ef1d212dcf605a78015137d4c33e916e2a6983e2
inform2vsr <35615430+inform2vsr@users.noreply.github.com>
2019-05-14 22:22:09 -0700

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //this is first app
+            //Editing this application for testing purpose.
         }
     }
 }
